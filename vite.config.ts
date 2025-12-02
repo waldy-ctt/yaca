057351c26194a249d6f99e551778d2fd8e522b5e
waldy_ctt <clocktoktok@gmail.com>
2025-12-02 09:01:34 +0700

    init

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import path, { resolve } from "node:path";
+import electron from "vite-plugin-electron/simple";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    electron({
+      main: {
+        entry: "electron/main.ts",
+      },
+      preload: {
+        input: path.join(__dirname, "electron/preload.ts"),
+      },
+      renderer:
+        process.env.NODE_ENV === "test"
+          ? undefined
+          : {},
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@renderer": resolve(__dirname, "src/renderer/src"),
+      "@": resolve(__dirname, "src/renderer/src"),
+    },
+  },
+});
